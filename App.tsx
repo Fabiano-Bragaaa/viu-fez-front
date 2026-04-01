ba1d73ae6aa453e6b4b2aee7f7af9b3d97c0894c
fabiano-bragaaa <fabianobragadev@gmail.com>
2026-03-31 23:35:21 -0300

    refactor: standardize import statements and ensure global styles are applied

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
+import "./global.css";
+
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import "./global.css";
 
 export default function App() {
   return (
