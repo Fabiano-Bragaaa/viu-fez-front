68a8c394a185cbe5c7be641c6cd1d886bf28f67d
fabiano-bragaaa <fabianobragadev@gmail.com>
2026-04-06 12:00:25 -0300

    feat: integrate HeroUI and update app structure with GestureHandlerRootView

--- App.tsx
+++ App.tsx
@@ -2,12 +2,16 @@ import "./global.css";
 
 import { Routes } from "@routes";
 import { StatusBar } from "expo-status-bar";
+import { HeroUINativeProvider } from "heroui-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function App() {
   return (
-    <>
-      <Routes />
-      <StatusBar style="auto" />
-    </>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <HeroUINativeProvider>
+        <Routes />
+        <StatusBar style="auto" />
+      </HeroUINativeProvider>
+    </GestureHandlerRootView>
   );
 }
