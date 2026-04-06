90f8141e7987bd3aa581d081adf03af0f2ddd977
fabiano-bragaaa <fabianobragadev@gmail.com>
2026-04-06 15:10:29 -0300

    feat: create text preset component for consistent text styling

--- src/components/text/text.tsx
+++ src/components/text/text.tsx
@@ -1,10 +1,9 @@
 import { getFontFamily } from '@utils';
 import type { VariantProps } from 'class-variance-authority';
-import React from 'react';
 import { Text as RNText } from 'react-native';
 import { twMerge } from 'tailwind-merge';
 
-import { textStyle } from './styles';
+import { textStyle } from './text-preset';
 
 type TextVariants = VariantProps<typeof textStyle>;
 
@@ -16,48 +15,38 @@ type TextProps = Omit<React.ComponentProps<typeof RNText>, 'children'> &
     family?: 'heading' | 'body';
   };
 
-const Text = React.forwardRef<React.ComponentRef<typeof RNText>, TextProps>(
-  function Text(
-    {
-      className,
-      isTruncated,
-      bold,
-      underline,
-      strikeThrough,
-      size = 'body-md',
-      italic,
-      align = 'left',
-      weight = 'normal',
-      family = 'body',
-      ...props
-    },
-    ref
-  ) {
-    const fontFamily = getFontFamily(weight, family);
+export function Text({
+  className,
+  isTruncated,
+  bold,
+  underline,
+  strikeThrough,
+  size = 'body-md',
+  italic,
+  align = 'left',
+  weight = 'normal',
+  family = 'body',
+  ...props
+}: TextProps) {
+  const fontFamily = getFontFamily(weight, family);
 
-    const computedClassName = textStyle({
-      isTruncated,
-      bold,
-      underline,
-      strikeThrough,
-      size,
-      italic,
-    });
+  const computedClassName = textStyle({
+    isTruncated,
+    bold,
+    underline,
+    strikeThrough,
+    size,
+    italic,
+  });
 
-    return (
-      <RNText
-        style={{
-          fontFamily,
-          textAlign: align,
-        }}
-        className={twMerge(computedClassName, className)}
-        {...props}
-        ref={ref}
-      />
-    );
-  }
-);
-
-Text.displayName = 'Text';
-
-export { Text };
+  return (
+    <RNText
+      style={{
+        fontFamily,
+        textAlign: align,
+      }}
+      className={twMerge(computedClassName, className)}
+      {...props}
+    />
+  );
+}
