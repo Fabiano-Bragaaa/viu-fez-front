4d070b54e61824d281e644c2bc6e0475bbf2e0a1
fabiano-bragaaa <fabianobragadev@gmail.com>
2026-05-01 17:38:41 -0300

    feat: wire up QueryClientProvider and Google sign in on login screen

--- App.tsx
+++ App.tsx
@@ -1,17 +1,22 @@
 import "./global.css";
 
 import { Routes } from "@routes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StatusBar } from "expo-status-bar";
 import { HeroUINativeProvider } from "heroui-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
+const queryClient = new QueryClient();
+
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <HeroUINativeProvider>
-        <Routes />
-        <StatusBar style="auto" />
-      </HeroUINativeProvider>
+      <QueryClientProvider client={queryClient}>
+        <HeroUINativeProvider>
+          <Routes />
+          <StatusBar style="auto" />
+        </HeroUINativeProvider>
+      </QueryClientProvider>
     </GestureHandlerRootView>
   );
 }
